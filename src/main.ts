57e7da50d5a6fe5fd799a31952a9e32b9d67a4bb
subeipo <suyhem@163.com>
2026-02-28 13:27:03 +0800

    feat: 添加message的依赖按需导入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
 import "./main.css";
 import App from "./App.vue";
 
